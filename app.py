e08e0c19797248653cf31e601263ee3e07cb1c0b
Pedro Leite Campos <pleitecampos@gmail.com>
2026-08-28 14:18:33 -0300

    adição de loops

--- app.py
+++ app.py
@@ -14,5 +14,15 @@ print ("4. Sair \n")
 
 opcao_escolhida = int (input ("Escolha uma opcao: "))
 
-print (f"Você escolheu a opcao {opcao_escolhida}")
+if (opcao_escolhida == 1):
+    print (f"Você escolheu cadastrar restaurante!")
+elif (opcao_escolhida == 2):
+    print (f"Você escolheu listar restaurantes!")
+elif (opcao_escolhida == 3):
+    print (f"Você escolheu ativar restaurante")
+elif (opcao_escolhida == 4):
+    print (f"Você decidiu sair!")
+else:
+    print ("Valor inválido!")
+
 
