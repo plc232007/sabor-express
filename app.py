f0b1374e640b176fd437a9a47bcad63a22039430
Pedro Leite Campos <pleitecampos@gmail.com>
2026-08-29 01:47:46 -0300

    Criação de funções para o projeto ficar mais modularizado.

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
-print ("""
+import os
+
+def exibir_nome_do_programa():
+
+    print ("""
 
 ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
 ██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
@@ -7,22 +11,49 @@ print ("""
 ██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║  ███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝
 ╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░ """)
 
-print ("1. Cadastrar restaurante")
-print ("2. Listar restaurante")
-print ("3. Ativar restaurante")
-print ("4. Sair \n")
+def exibir_opcoes():
 
-opcao_escolhida = int (input ("Escolha uma opcao: "))
+    print ("1. Cadastrar restaurante")
+    print ("2. Listar restaurante")
+    print ("3. Ativar restaurante")
+    print ("4. Sair \n")
 
-if (opcao_escolhida == 1):
-    print (f"Você escolheu cadastrar restaurante!")
-elif (opcao_escolhida == 2):
-    print (f"Você escolheu listar restaurantes!")
-elif (opcao_escolhida == 3):
-    print (f"Você escolheu ativar restaurante")
-elif (opcao_escolhida == 4):
-    print (f"Você decidiu sair!")
-else:
-    print ("Valor inválido!")
+def finalizar_app():
+    os.system('clear')
+    print ('Finalizando o app')
+
+def opcao_invalida():
+    print ("Opção Inválida!")
+    input ("Digite uma tecla para voltar ao menu pricipal: ")
+    main()
+
+
+def escolher_opcao():
+    try: # assim o código não quebra, ele apresenta para o user uma mensagem em caso de erro.
+        opcao_escolhida = int(input('Escolha uma opção: '))
+        match opcao_escolhida:
+            case 1:
+                print('Adicionar restaurante')
+            case 2:
+                print('Listar restaurantes')
+            case 3:
+                print('Ativar restaurante')
+            case 4:
+                finalizar_app()
+            case _:
+                opcao_invalida ()
+    except:
+        opcao_invalida()
+
+def main():
+    os.system('clear')
+    exibir_nome_do_programa()
+    exibir_opcoes()
+    escolher_opcao()
+
+
+
+if __name__ == '__main__':
+    main()
 
 
